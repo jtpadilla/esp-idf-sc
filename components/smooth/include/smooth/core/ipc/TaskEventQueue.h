56d4140f1879e202df69d9fa1804f9834a546305
Juan Padilla <jtpadilla@gmail.com>
2020-07-09 13:58:04 +0200

    Estudio

--- components/smooth/include/smooth/core/ipc/TaskEventQueue.h
+++ components/smooth/include/smooth/core/ipc/TaskEventQueue.h
@@ -1,20 +1,3 @@
-/*
-Smooth - A C++ framework for embedded programming on top of Espressif's ESP-IDF
-Copyright 2019 Per Malmberg (https://gitbub.com/PerMalmberg)
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 #pragma once
 
 #include "smooth/core/Task.h"
@@ -26,6 +9,7 @@ limitations under the License.
 
 namespace smooth::core::ipc
 {
+
     /// TaskEventQueue expands the functionality of the Queue<T> by, together with the Task, adding the ability
     /// to signal a Task when an item is available, making polling a queue unnecessary which frees up the task
     /// to do other things.
@@ -43,8 +27,7 @@ namespace smooth::core::ipc
 
             static auto create(int size, Task& owner_task, IEventListener<T>& event_listener)
             {
-                return smooth::core::util::create_protected_shared<TaskEventQueue<T>>(size, owner_task,
-                                                                                      event_listener);
+                return smooth::core::util::create_protected_shared<TaskEventQueue<T>>(size, owner_task, event_listener);
             }
 
             ~TaskEventQueue() override
@@ -150,4 +133,5 @@ namespace smooth::core::ipc
             Task& task;
             IEventListener<T>& listener;
     };
+
 }
